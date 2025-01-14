331427d4cd28c9550e5bd9b5a42c9c506aa8d7e8
Rich <rs2585@cornell.edu>
2025-01-13 23:57:20 -0500

    setup additions, add runpodctl scripts

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@ if [ ! -d "logs" ]; then
     mkdir logs
 fi
 
+wget -qO- cli.runpod.net | sudo bash
+runpod_api_key=$(<runpod_api_key)
+runpodctl config --apiKey $runpod_api_key
+
+sudo dnf install cronie
+
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
