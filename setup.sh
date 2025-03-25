0940a914fa1a088c279134bafe9d0de6d2633e4a
Rich <rs2585@cornell.edu>
2025-03-24 23:55:21 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ if [ ! -d "logs" ]; then
     mkdir logs
 fi
 
+sudo dnf install crontab tmux rsync vim -y
+
+new_cron_job="0 3 * * * /path/to/your_script.sh"
+(crontab -l 2>/dev/null; echo "$new_cron_job") | crontab -
+
 wget -qO- cli.runpod.net | sudo bash
 runpod_api_key=$(<runpod_api_key)
 runpodctl config --apiKey $runpod_api_key
