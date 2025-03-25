f7959413547de6d6fc1349ae6ce9559f0218958e
Rich <rs2585@cornell.edu>
2025-03-25 00:12:34 -0400

    update readme, setup, etc.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # bigkahuna
 Host to control workers.
 
-- Run with `nohup bash run_server.sh &` to run in the background.
-- `ps aux | grep python`
-
-
-Needs a runpod key and token that matches what clients will use
+## Setup
+1. Run `bash setup.sh` to install and setup everything that's needed
+2. Start the server with `nohup bash run_server.sh &` to run in the background.
+3. To kill it, find it using `ps aux | grep python` and kill the instance
+4. Transfer the runpod_api_key.txt and the flask secret token.txt files to the repo
--- setup.sh
+++ setup.sh
@@ -4,13 +4,17 @@ if [ ! -d "logs" ]; then
     mkdir logs
 fi
 
-sudo dnf install crontab tmux rsync vim -y
+sudo dnf install cronie tmux rsync vim -y
 
-new_cron_job="0 3 * * * /path/to/your_script.sh"
-(crontab -l 2>/dev/null; echo "$new_cron_job") | crontab -
+launch_cron_job="14 * * * launch_runpod_instance.sh"
+(crontab -l 2>/dev/null; echo "$launch_cron_job") | crontab -
+terminate_cron_job="18 * * * terminate_runpod_instance.sh"
+(crontab -l 2>/dev/null; echo "$terminate_cron_job") | crontab -
+
+export EDITOR=vim
 
 wget -qO- cli.runpod.net | sudo bash
-runpod_api_key=$(<runpod_api_key)
+runpod_api_key=$(<runpod_api_key.txt)
 runpodctl config --apiKey $runpod_api_key
 
 python3 -m venv .venv
